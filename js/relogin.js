33136cc63641d42db3d996ea56cca86ee2953d98
bruggi07 <ruggiabruno07@gmail.com>
2023-03-22 05:24:46 -0300

    Pág relogin completa, fix backgrounds botones general

---
+++ js/relogin.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	let load = document.getElementById('loading');
+	let contcarga = document.getElementById('contcarga');
+	load.classList.add('cargahide');
+	contcarga.classList.add('imghide');
+}
