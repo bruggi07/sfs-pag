652e0b662cdb4177ccc911f564dd986a319d7dc9
bruggi07 <ruggiabruno07@gmail.com>
2023-05-15 21:05:54 -0300

    Login terminado en toda la web y todas las páginas

---
+++ js/accesofb.js
@@ -0,0 +1,4 @@
+document.getElementById('incorrectoCerrarfb').addEventListener('click',()=>{
+    document.querySelector('#result').classList.remove('show');
+	document.querySelector('#accesoform').classList.add('show');
+})
