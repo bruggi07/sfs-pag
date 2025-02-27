f761a67ef92ee9cf8609c1250ff4de6ea281ff55
bruggi07 <ruggiabruno07@gmail.com>
2025-02-27 13:29:16 -0300

    ****

--- js/modules/form_acceso.js
+++ js/modules/form_acceso.js
@@ -1,31 +1,31 @@
 // formu acceso -------->
 import { open_acceso, form_acceso, close_acceso, opacoacceso, open_acceso_mobil, btn__enviar_acceso } from './model.js';
 
-// open_acceso.addEventListener('click',()=>{
-// 	window.location.href = "https://online2.sfssa.com.ar/SFSFrameworkRest/#/login";
-// })
+open_acceso.addEventListener('click',()=>{
+	window.location.href = "https://online.sfssa.com.ar/";
+})
 
-open_acceso.addEventListener('click', () => {
-	form_acceso.classList.remove('oculto-acc');
-	opacoacceso.classList.add('opaco');
-});
-open_acceso_mobil.addEventListener('click', () => {
-	form_acceso.classList.remove('oculto-acc');
-	opacoacceso.classList.add('opaco');
-});
-close_acceso.addEventListener('click', () => {
-	form_acceso.classList.add('oculto-acc');
-	opacoacceso.classList.remove('opaco');
-});
-opacoacceso.addEventListener('click', () => {
-	form_acceso.classList.add('oculto-acc');
-	opacoacceso.classList.remove('opaco');
-});
+// open_acceso.addEventListener('click', () => {
+// 	form_acceso.classList.remove('oculto-acc');
+// 	opacoacceso.classList.add('opaco');
+// });
+// open_acceso_mobil.addEventListener('click', () => {
+// 	form_acceso.classList.remove('oculto-acc');
+// 	opacoacceso.classList.add('opaco');
+// });
+// close_acceso.addEventListener('click', () => {
+// 	form_acceso.classList.add('oculto-acc');
+// 	opacoacceso.classList.remove('opaco');
+// });
+// opacoacceso.addEventListener('click', () => {
+// 	form_acceso.classList.add('oculto-acc');
+// 	opacoacceso.classList.remove('opaco');
+// });
 // btn__enviar_acceso.addEventListener('click',(e)=>{
 // 	e.preventDefault();
 // 	validar();
 // })
-document.querySelector('#incorrectoCerrarAcc').addEventListener('click', () => {
-	document.querySelector('#result').classList.remove('show');
-	document.querySelector('#accesoform').classList.add('show');
-})
+// document.querySelector('#incorrectoCerrarAcc').addEventListener('click', () => {
+// 	document.querySelector('#result').classList.remove('show');
+// 	document.querySelector('#accesoform').classList.add('show');
+// })
